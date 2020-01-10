d4dc27aac209ac8d45eadda35cfd8f626d815143
Jack Parker <jprkr96@gmail.com>
2020-01-10 15:02:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,10 @@ end
 
 def update_counting_hash(hash, key)
  if hash[key]
-    p hash[key] += 1
+    hash[key] += 1
+    return hash
  else
-    p hash[key] = 1
+    hash[key] = 1
+    return hash
  end
 end
