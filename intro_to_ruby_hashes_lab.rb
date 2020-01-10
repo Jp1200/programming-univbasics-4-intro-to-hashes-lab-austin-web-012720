c6186adf0f89eefe8be6ee7fc043f59b21d1925a
Jack Parker <jprkr96@gmail.com>
2020-01-10 14:44:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,13 @@
 def new_hash
-  
+  hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  jack = {age: 23, race: "white"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  
 end
 
 def id_generator
